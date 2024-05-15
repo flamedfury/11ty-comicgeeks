81e9ad50b2f9825fefc14a1659df1de7535caf39
Chris Burnell <me@chrisburnell.com>
2024-05-15 23:53:11 +0800

    fix: only get run fetchComics when 11ty is building (not serving)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,17 @@
-const __siteroot = __dirname.replace("/cli", "");
+require('dotenv').config()
 
+const __siteroot = __dirname.replace("/cli", "");
 const fetchComics = (...args) => import('./cli/commands/fetchComics.js').then(({default: fetchComics}) => fetchComics(...args));
 
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy ("src/assets/css/styles.css");
 
-  eleventyConfig.on("beforeBuild", async () => {
-    await fetchComics(__siteroot);
-  });
+  if (process.env.ELEVENTY_RUN_MODE === "build") {
+    eleventyConfig.on("beforeBuild", async () => {
+        await fetchComics(__siteroot);
+    });
+  }
 
   return {
     dir: {
